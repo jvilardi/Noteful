238348c0560293612ac890526b48281206a09972
jvilardi <justin.vilardi@gmail.com>
2015-01-19 21:21:15 +0000

    These are a bunch of changes

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,5 +5,13 @@ class Note < ActiveRecord::Base
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
+  
+  def preview
+    if self.content.length > 100
+      self.content.slice(0,100) << " ...(read more)"
+    else
+      self.content
+    end
+  end
 
 end
